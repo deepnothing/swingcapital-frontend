51e94037696dd26e5b5e0cdf7d6d1f25a51002bd
teo <teo@nudgecoach.com>
2023-02-07 17:16:20 -0500

    change slug name

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 export default {
   "expo": {
-    "name": "legal-call",
-    "slug": "legal-call",
+    "name": "swingcapital",
+    "slug": "swingcapital",
     "privacy": "public",
     "version": "1.0.0",
     "orientation": "portrait",
