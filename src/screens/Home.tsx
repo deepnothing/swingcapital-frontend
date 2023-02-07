643d57864cb7a6dc681121a64aab29cece2d3c8b
teo <teo@nudgecoach.com>
2023-02-07 12:27:48 -0500

    add topbar

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Text, View,SafeAreaView} from 'react-native';
-import SwingCapital from '../components/SwingCapital'
-
+import React from "react";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
+import SwingCapital from "../components/SwingCapital";
 
 export default function HomeScreen() {
-
   return (
     <SafeAreaView className="w-full h-full">
-      <View className="pl-4 pt-3 pb-3 border-b w-full flex justify-center align-center">
-        <SwingCapital/>
+      <View style={styles.topBar}>
+        <SwingCapital />
       </View>
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  topBar: {
+    marginHorizontal: 12,
+    marginTop:10,
+    borderRadius:6,
+    padding:15,
+    paddingTop:19,
+    backgroundColor:'#ffc72c',
+    shadowColor: 'rgba(0, 0, 0, 0.45)',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 1,
+    shadowRadius:5,
+    
+  },
+});
