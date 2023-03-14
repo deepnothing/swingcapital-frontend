5ce0a77d50ce2de83c28d679510821a181326e95
teo <teo@nudgecoach.com>
2023-03-14 15:24:38 -0400

    add delete account alert

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -8,6 +8,7 @@ import {
   SafeAreaView,
   StyleSheet,
   Switch,
+  Alert,
 } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 import IonIcon from "react-native-vector-icons/Ionicons";
@@ -23,12 +24,29 @@ function Settings() {
   const { user } = useAuth();
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-
+  const deleteAccount = (user) => {
+    Alert.alert(
+      "",
+      "You are about to delete your account, this action cannot be undone",
+      [
+        {
+          text: "Cancel",
+          style: "destructive",
+        },
+        {
+          text: "Delete",
+          onPress: () => deleteUser(user),
+          style: "destructive",
+        },
+      ]
+    );
+    // deleteUser(user)
+  };
   return (
     <View>
       <Header>{/* <SwingCapitalText text="Settings" /> */}</Header>
       <View style={styles.options}>
-      <View style={styles.row}>
+        <View style={styles.row}>
           <TouchableOpacity>
             <Feather
               name="user"
@@ -37,17 +55,15 @@ function Settings() {
               style={{ marginRight: 10 }}
             />
           </TouchableOpacity>
-          <Text style={{fontWeight:'600'}}>{user?.email}</Text>
+          <Text style={{ fontWeight: "600" }}>{user?.email}</Text>
         </View>
         <View style={styles.row}>
-          <TouchableOpacity onPress={() => signOut(auth)}>
-            <Feather
-              name="sun"
-              color="black"
-              size={"25"}
-              style={{ marginRight: 10 }}
-            />
-          </TouchableOpacity>
+          <Feather
+            name="sun"
+            color="black"
+            size={"25"}
+            style={{ marginRight: 10 }}
+          />
           <Text>Theme </Text>
           <Switch
             style={{ transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }] }}
@@ -58,28 +74,28 @@ function Settings() {
             value={isEnabled}
           />
         </View>
-        <View style={styles.row}>
-          <TouchableOpacity onPress={() => signOut(auth)}>
-            <Feather
-              name="log-out"
-              color="black"
-              size={"25"}
-              style={{ marginRight: 10 }}
-            />
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.row} onPress={() => signOut(auth)}>
+          <Feather
+            name="log-out"
+            color="black"
+            size={"25"}
+            style={{ marginRight: 10 }}
+          />
           <Text>Logout</Text>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity onPress={() => signOut(auth)}>
-            <Feather
-              name="x-octagon"
-              color="red"
-              size={"25"}
-              style={{ marginRight: 10 }}
-            />
-          </TouchableOpacity>
-          <Text>Delete Account </Text>
-        </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() => deleteAccount(auth)}
+        >
+          <Feather
+            name="x-octagon"
+            color="red"
+            size={"25"}
+            style={{ marginRight: 10 }}
+          />
+
+          <Text>Delete Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
