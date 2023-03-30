83b2f8d125af5d06256b276cadb534ab3cc23099
teo <teo@nudgecoach.com>
2023-03-30 01:52:48 -0400

    map sizing updates and map banner

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View, Text } from "react-native";
 import Svg, { G, Path, Circle } from "react-native-svg";
 import * as d3 from "d3";
 import { geoCylindricalStereographic } from "d3-geo-projection";
@@ -9,6 +9,7 @@ import { COUNTRIES } from "./CountryShapes";
 const Map = (props) => {
   const { dimensions, routeColor, data } = props;
   const [countryList, setCountryList] = useState([]);
+  const [clickedCountry, setClickedCountry] = useState();
   const mapExtent = useMemo(() => {
     return dimensions.width > dimensions.height / 2
       ? dimensions.height / 2
@@ -16,8 +17,8 @@ const Map = (props) => {
   }, [dimensions]);
   const countryPaths = useMemo(() => {
     const projection = geoCylindricalStereographic()
-      .translate([dimensions.width / 2, dimensions.height / 8])
-      .scale(dimensions.width / 4);
+      .translate([dimensions.width / 0.31, dimensions.height / 1])
+      .scale(dimensions.width);
 
     const geoPath = d3.geoPath().projection(projection);
     const svgPaths = COUNTRIES.map(geoPath);
@@ -39,9 +40,11 @@ const Map = (props) => {
             // y={50}
             onClick={() => {
               console.log(COUNTRIES[i].properties.name);
+              setClickedCountry(COUNTRIES[i].properties.name);
             }}
             onClickRelease={() => {
               console.log("onClickRelease!");
+              setClickedCountry(null);
             }}
           >
             <Path
@@ -70,23 +73,58 @@ const Map = (props) => {
     <View
       style={{
         borderWidth: 3,
-        height: dimensions.height / 3,
+        height: dimensions.height / 3.5,
         width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        borderRadius: 8,
       }}
     >
+      {clickedCountry ? (
+        <View
+          style={{
+            position: "absolute",
+            height: 20,
+            backgroundColor: "#fff",
+            paddingHorizontal:10,
+            top: 0,
+            left: 0,
+            zIndex: 1,
+          }}
+        >
+          <Text>
+            {clickedCountry}:
+            {data.find((item) => item.geoName === clickedCountry)?.geoName
+              ? data.find((item) => item.geoName === clickedCountry).value[0]
+              : 0}
+          </Text>
+        </View>
+      ) : null}
+
       <SvgPanZoom
-        canvasHeight={500}
-        canvasWidth={dimensions.width}
+        canvasWidth={2700}
+        canvasHeight={1500}
         minScale={0.1}
-        initialZoom={0.7}
+        initialZoom={0.143}
         onZoom={(zoom) => {
           console.log("onZoom:" + zoom);
         }}
-        canvasStyle={{ borderWidth: 2, height: "100%", width: "100%" }}
-        viewStyle={{ backgroundColor: "green", height: "100%", width: "100%" }}
+        canvasStyle={{
+          backgroundColor: "yellow",
+          position: "absolute",
+          left: "2%",
+          top: "5%",
+          // height: "100%",
+          //height: dimensions.height / 2,
+        }}
+        viewStyle={{
+          backgroundColor: "green",
+          position: "relative",
+          height: "100%",
+          width: "100%",
+          overflow: "hidden",
+        }}
       >
         <G>{countryList.map((x) => x)}</G>
       </SvgPanZoom>
