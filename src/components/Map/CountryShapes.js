c1c5364b4bff6d631a61e69366c79d08545d82d5
teo <teo@nudgecoach.com>
2023-04-30 22:52:47 -0400

    add some flags

--- src/components/Map/CountryShapes.js
+++ src/components/Map/CountryShapes.js
@@ -6,122 +6,124 @@ export const COUNTRIES = feature(
   countries.objects.countries
 ).features;
 
+
+// flag name list
 [
-  "Fiji",
-  "Tanzania",
-  "Western Sahara",
-  "Canada",
-  "United States",
-  "Kazakhstan",
-  "Uzbekistan",
-  "Papua New Guinea",
-  "Indonesia",
-  "Argentina",
-  "Chile",
+  "Fiji",//
+  "Tanzania",//
+  "Western Sahara",//
+  "Canada",//
+  "United States",//
+  "Kazakhstan",//
+  "Uzbekistan",//
+  "Papua New Guinea",//
+  "Indonesia",//
+  "Argentina",//
+  "Chile",//
   "Congo - Kinshasa",
-  "Somalia",
-  "Kenya",
-  "Sudan",
-  "Chad",
-  "Haiti",
-  "Dominican Republic",
-  "Russia",
-  "Bahamas",
+  "Somalia",//
+  "Kenya",//
+  "Sudan",//
+  "Chad",//
+  "Haiti",//
+  "Dominican Republic",//
+  "Russia",//
+  "Bahamas",//
   "Falkland Islands (Islas Malvinas)",
-  "Norway",
-  "Greenland",
+  "Norway",//
+  "Greenland",//
   "Fr. S. Antarctic Lands",
   "Timor-Leste",
-  "South Africa",
-  "Lesotho",
-  "Mexico",
-  "Uruguay",
-  "Brazil",
-  "Bolivia",
-  "Peru",
-  "Colombia",
-  "Panama",
-  "Costa Rica",
-  "Nicaragua",
-  "Honduras",
-  "El Salvador",
-  "Guatemala",
-  "Belize",
-  "Venezuela",
-  "Guyana",
-  "Suriname",
-  "France",
-  "Ecuador",
-  "Puerto Rico",
-  "Jamaica",
-  "Cuba",
-  "Zimbabwe",
-  "Botswana",
-  "Namibia",
-  "Senegal",
-  "Mali",
-  "Mauritania",
-  "Benin",
-  "Niger",
-  "Nigeria",
-  "Cameroon",
-  "Togo",
-  "Ghana",
-  "Côte d’Ivoire",
-  "Guinea",
-  "Guinea-Bissau",
-  "Liberia",
-  "Sierra Leone",
-  "Burkina Faso",
-  "Central African Republic",
-  "Congo - Brazzaville",
-  "Gabon",
-  "Equatorial Guinea",
-  "Zambia",
-  "Malawi",
-  "Mozambique",
-  "Eswatini",
-  "Angola",
-  "Burundi",
-  "Israel",
-  "Lebanon",
-  "Madagascar",
-  "Palestine",
-  "Gambia",
+  "South Africa",//
+  "Lesotho",//
+  "Mexico",//
+  "Uruguay",//
+  "Brazil",//
+  "Bolivia",//
+  "Peru",//
+  "Colombia",//
+  "Panama",//
+  "Costa Rica",//
+  "Nicaragua",//
+  "Honduras",//
+  "El Salvador",//
+  "Guatemala",//
+  "Belize",//
+  "Venezuela",//
+  "Guyana",//
+  "Suriname",//
+  "France",//
+  "Ecuador",//
+  "Puerto Rico",//
+  "Jamaica",//
+  "Cuba",//
+  "Zimbabwe",//
+  "Botswana",//
+  "Namibia",//
+  "Senegal",//
+  "Mali",//
+  "Mauritania",//
+  "Benin",//
+  "Niger",//
+  "Nigeria",//
+  "Cameroon",//
+  "Togo",//
+  "Ghana",//
+  "Côte d’Ivoire",//
+  "Guinea",//
+  "Guinea-Bissau",//
+  "Liberia",//
+  "Sierra Leone",//
+  "Burkina Faso",//
+  "Central African Republic",//
+  "Congo - Brazzaville",//
+  "Gabon",//
+  "Equatorial Guinea",//
+  "Zambia",//
+  "Malawi",//
+  "Mozambique",//
+  "Eswatini",//
+  "Angola",//
+  "Burundi",//
+  "Israel",//
+  "Lebanon",//
+  "Madagascar",//
+  "Palestine",//
+  "Gambia",//
   "Tunisia",
-  "Algeria",
-  "Jordan",
-  "United Arab Emirates",
-  "Qatar",
-  "Kuwait",
-  "Iraq",
-  "Oman",
-  "Vanuatu",
-  "Cambodia",
-  "Thailand",
-  "Laos",
-  "Myanmar (Burma)",
-  "Vietnam",
-  "North Korea",
-  "South Korea",
-  "Mongolia",
-  "India",
-  "Bangladesh",
-  "Bhutan",
-  "Nepal",
-  "Pakistan",
-  "Afghanistan",
-  "Tajikistan",
-  "Kyrgyzstan",
-  "Turkmenistan",
-  "Iran",
-  "Syria",
-  "Armenia",
-  "Sweden",
-  "Belarus",
-  "Ukraine",
-  "Poland",
-  "Austria",
+  "Algeria",//
+  "Jordan",//
+  "United Arab Emirates",//
+  "Qatar",//
+  "Kuwait",//
+  "Iraq",//
+  "Oman",//
+  "Vanuatu",//
+  "Cambodia",//
+  "Thailand",//
+  "Laos",//
+  "Myanmar (Burma)",//
+  "Vietnam",//
+  "North Korea",//
+  "South Korea",//
+  "Mongolia",//
+  "India",//
+  "Bangladesh",//
+  "Bhutan",//
+  "Nepal",//
+  "Pakistan",//
+  "Afghanistan",//
+  "Tajikistan",//
+  "Kyrgyzstan",//
+  "Turkmenistan",//
+  "Iran",//
+  "Syria",//
+  "Armenia",//
+  "Sweden",//
+  "Belarus",//
+  "Ukraine",//
+  "Poland",//
+  "Austria",//
   "Hungary",
   "Moldova",
   "Romania",
