9b729e418aadee2701cd062339bb03033f4fa316
teo <teo@nudgecoach.com>
2023-06-09 00:10:49 -0400

    add api keys to .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 import "firebase/auth";
+import dotenv from "dotenv";
+
+// Load the environment variables from .env file
+dotenv.config();
+
 // import Constants from "expo-constants";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,24 +14,16 @@ import "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCK72VQp2c7MCS7Z9pLjYCmWzUZUvvq8Xg",
-  authDomain: "swingcapital-9e531.firebaseapp.com",
-  projectId: "swingcapital-9e531",
-  storageBucket: "swingcapital-9e531.appspot.com",
-  messagingSenderId: "1095569946706",
-  appId: "1:1095569946706:web:83f2962ce95b8f41e6478b",
-  measurementId: "G-H5S50G07W5",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
-
-// function writeUserData(email) {
-//   const reference = ref(db, "users/" + userId);
-//   set(reference, {
-//     email: email,
-//   });
-// }
-
